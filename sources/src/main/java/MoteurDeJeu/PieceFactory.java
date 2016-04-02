f48f4c31e7d6b5d8e8889e7cd689923b67a7f973
maxxxiii <fernandez@et.esiea.fr>
2016-04-02 11:55:11 +0200

    ajout des pieces

--- sources/src/main/java/MoteurDeJeu/PieceFactory.java
+++ sources/src/main/java/MoteurDeJeu/PieceFactory.java
@@ -1,4 +1,21 @@
 package MoteurDeJeu;
+import java.util.Random;
 
 public class PieceFactory {
+    PieceFactory() {
+        //generation nombre random
+        int[][] newPiece = new int[4][16];
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 16; j++) {
+                    newPiece[i][j] = 0;
+
+                }
+            }
+
+
+        Random rand = new Random();
+        int nombre = rand.nextInt(7) + 1;
+
+
+    }
 }
